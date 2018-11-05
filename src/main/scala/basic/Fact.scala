39728e8b195d9e2fe6cf6761a71ca1e663d25678
JojiKoike <joji.koike@gmail.com>
2018-11-05 12:37:27 +0900

    Recursive Implement Sample : Factorial

---
+++ src/main/scala/basic/Fact.scala
@@ -0,0 +1,18 @@
+package basic
+
+object Fact extends App {
+  import scala.io.StdIn._
+
+  val n = readLine.toInt
+  println(fact(n))
+
+  def fact(n: Int): Int = {
+    if (n == 0) {
+      1
+    } else {
+      fact( n - 1 ) * n
+    }
+  }
+
+}
+
