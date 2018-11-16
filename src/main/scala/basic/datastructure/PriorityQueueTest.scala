99f137b0cc07e08425e134b95af105fb7ff94512
JojiKoike <joji.koike@gmail.com>
2018-11-16 14:40:41 +0900

    Priority Queue for case Class

--- src/main/scala/basic/datastructure/PriorityQueueTest.scala
+++ src/main/scala/basic/datastructure/PriorityQueueTest.scala
@@ -23,4 +23,19 @@ object PriorityQueueTest extends App {
   println()
   pq_asc.dequeueAll.mkString(" ").foreach(print)
 
+  // Build Task Queue
+  val taskqueue = new mutable.PriorityQueue[Task]()(Ordering.by(taskOrder))
+  val n = readLine.toInt
+  val tasks = for (i <- 0 until n) yield readLine.split(" ").map(_.toInt)
+  for (task <- tasks) {
+    taskqueue.enqueue(Task(task(0), task(1)))
+  }
+  taskqueue.dequeueAll.foreach(println)
+
+  case class Task(start: Int, end: Int)
+  def taskOrder(task : Task) : Int = -task.end
+
 }
+
+
+
