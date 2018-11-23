3970b83093ea17f7da36a51bb72c275e778caee1
JojiKoike <joji.koike@gmail.com>
2018-11-23 21:50:14 +0900

    Expedition (POJ 2431)

---
+++ src/main/scala/basic/graph/Expedition.scala
@@ -0,0 +1,48 @@
+package basic.graph
+
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+
+object Expedition extends App {
+  import scala.io.StdIn._
+
+  // Get Input Values
+  val Array(n, l, p) = readLine.split(" ").map(_.toInt)
+  val a = readLine.split(" ").map(_.toInt)
+  val b = readLine.split(" ").map(_.toInt)
+
+  // Build Supply Station Info Array
+  val A = ArrayBuffer.empty[Int]
+  val B = ArrayBuffer.empty[Int]
+  for (i <- a.indices) {
+    A.append(a(i))
+    B.append(b(i))
+  }
+  A.append(l)
+  B.append(0)
+
+  val pq = new mutable.PriorityQueue[Int]()
+
+  var ans = 0
+  var curPos = 0
+  var tank = p
+  var isEmpty = false
+
+  for (i <- a.indices if !isEmpty) {
+    val d = a(i) - curPos
+    while (tank - d < 0 && !isEmpty) {
+      if (pq.isEmpty) {
+        isEmpty = true
+      } else {
+        tank += pq.dequeue()
+        ans += 1
+      }
+    }
+    if (!isEmpty) {
+      tank -= d
+      curPos = a(i)
+      pq.enqueue(b(i))
+    }
+  }
+  println(if (isEmpty) -1 else ans)
+}
