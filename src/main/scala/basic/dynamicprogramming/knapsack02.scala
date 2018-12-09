9ed530e2b3e108f26f9c5ac320f70bf8a8598c7a
JojiKoike <joji.koike@gmail.com>
2018-12-09 22:39:16 +0900

    Unlimited Knapsack, Refactor

---
+++ src/main/scala/basic/dynamicprogramming/knapsack02.scala
@@ -0,0 +1,49 @@
+package basic.dynamicprogramming
+
+
+object knapsack02 extends App {
+  import scala.io.StdIn._
+  import scala.collection.mutable.ArrayBuffer
+
+  // Step 1 : Get n : Number of items, w : Maximum sum of weight
+  val Array(n, w) = readLine.split(" ").map(_.toInt)
+
+  // Step 2 : Build Weight and Value Array
+  val weights = new ArrayBuffer[Int]()
+  val values = new ArrayBuffer[Int]()
+  for (i <- 0 until n) {
+    val wv = readLine.split(" ").map(_.toInt)
+    weights.append(wv(0))
+    values.append(wv(1))
+  }
+
+  // Step 3 : Initialize DP
+  var dp = ArrayBuffer.empty[Array[Int]]
+  for (i <- 0 to n) {
+    val row = new ArrayBuffer[Int]()
+    for (j <- 0 to n * values.max) {
+      row.append(1000000000)
+    }
+    dp.append(row.toArray)
+  }
+  dp(0)(0) = 0
+
+  // Step 4 : Solve
+  for (i <- 0 until n) {
+    for (j <- 0 to n * values.max) {
+      if (values(i) > j) {
+        dp(i + 1)(j) = dp(i)(j)
+      } else {
+        dp(i + 1)(j) = math.min(dp(i)(j), dp(i)(j - values(i)) + weights(i))
+      }
+    }
+  }
+
+  // Step 5 : Show Result
+  var res = 0
+  for (i <- 0 to n * values.max) {
+    if (dp(n)(i) <= w) res = i
+  }
+  printf("Result : %d\n", res)
+
+}
--- src/main/scala/basic/dynamicprogramming/unlimitedknapsack.scala
+++ src/main/scala/basic/dynamicprogramming/unlimitedknapsack.scala
@@ -1,49 +1,41 @@
 package basic.dynamicprogramming
 
+import scala.collection.mutable.ArrayBuffer
 
 object unlimitedknapsack extends App {
   import scala.io.StdIn._
-  import scala.collection.mutable.ArrayBuffer
 
-  // Step 1 : Get n : Number of items, w : Maximum sum of weight
-  val Array(n, w) = readLine.split(" ").map(_.toInt)
+  // Get Input Data
+  val n = readLine.toInt
+  val wv = for (i <- 0 until n) yield readLine.split(" ").map(_.toInt)
+  val W = readLine.toInt
 
-  // Step 2 : Build Weight and Value Array
-  val weights = new ArrayBuffer[Int]()
-  val values = new ArrayBuffer[Int]()
-  for (i <- 0 until n) {
-    val wv = readLine.split(" ").map(_.toInt)
-    weights.append(wv(0))
-    values.append(wv(1))
-  }
-
-  // Step 3 : Initialize DP
+  // Initialize DP Table
   var dp = ArrayBuffer.empty[Array[Int]]
-  for (i <- 0 to n) {
+  for (i <-0 to n) {
     val row = new ArrayBuffer[Int]()
-    for (j <- 0 to n * values.max) {
-      row.append(1000000000)
+    for (j <- 0 to W) {
+      row.append(0)
     }
     dp.append(row.toArray)
   }
   dp(0)(0) = 0
 
-  // Step 4 : Solve
+  // Solve
   for (i <- 0 until n) {
-    for (j <- 0 to n * values.max) {
-      if (values(i) > j) {
+    val w = wv(i)(0)
+    val v = wv(i)(1)
+    for (j <- 0 to W) {
+      if (j < w) {
         dp(i + 1)(j) = dp(i)(j)
       } else {
-        dp(i + 1)(j) = math.min(dp(i)(j), dp(i)(j - values(i)) + weights(i))
+        dp(i + 1)(j) = math.max(dp(i)(j), dp(i + 1)(j - w) + v)
       }
     }
   }
 
-  // Step 5 : Show Result
-  var res = 0
-  for (i <- 0 to n * values.max) {
-    if (dp(n)(i) <= w) res = i
-  }
-  printf("Result : %d\n", res)
+  // Show Result
+  printf("Result : %d\n", dp(n)(W))
+
 
 }
